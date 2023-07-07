b9f75d9dff59f2840d128080d1889ac9d86ad7fd
JiangYiJia6 <116914241+JiangYiJia6@users.noreply.github.com>
2023-07-07 17:46:35 -0400

    Initial Commit

---
+++ RecipeSaver/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecipeSaver
+//
+//  Created by Yijia Jiang on 2023-07-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipeSaver/RecipeSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSaverApp.swift
+//  RecipeSaver
+//
+//  Created by Yijia Jiang on 2023-07-07.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
